fffbf17b87734831c8404b84e6b13f92c0ce074a
Samuel Ogbodo <142540658+Samuel515@users.noreply.github.com>
2023-09-22 10:59:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector("button").addEventListener("click", function clickOrKeyDown(){
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 document.querySelector("body > div > div:nth-child(2) > img").setAttribute("src" , "images/dice"+ randomNumber1 +".png");
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector("body > div > div:nth-child(3) > img").setAttribute("src" , "images/dice"+ randomNumber2 +".png");
 
-if(randomNumber1 > randomNumber2){
+    if(randomNumber1 > randomNumber2){
     document.querySelector("body > div > h1").textContent = ("Player 1 wins!");
 }else if(randomNumber1 < randomNumber2){
     document.querySelector("body > div > h1").textContent = ("Player 2 wins!");
 }else{
     document.querySelector("body > div > h1").textContent = ("Draw!");
 }
+
+    document.querySelector("button").classList.add("clicked");
+    setTimeout (function(){
+        document.querySelector("button").classList.remove("clicked");
+    }, 100)
+
+});
